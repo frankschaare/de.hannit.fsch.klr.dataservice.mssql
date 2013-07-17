5324c92e28ae492c8f15d6ce7551ffea7d776438
frankschaare <frank.schaare@gmail.com>
2013-07-17 18:00:32 +0200

    20130717: Insert AZV abgeschlossen

--- de.hannit.fsch.klr.dataservice.mssql/src/de/hannit/fsch/klr/dataservice/mssql/MSSQLDataService.java
+++ de.hannit.fsch.klr.dataservice.mssql/src/de/hannit/fsch/klr/dataservice/mssql/MSSQLDataService.java
@@ -10,17 +10,20 @@ import java.io.InputStream;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Properties;
 
 import org.eclipse.core.runtime.FileLocator;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
+import de.hannit.fsch.common.csv.azv.AZVDatensatz;
 import de.hannit.fsch.common.loga.LoGaDatensatz;
 import de.hannit.fsch.common.mitarbeiter.Mitarbeiter;
 import de.hannit.fsch.klr.dataservice.DataService;
@@ -39,6 +42,8 @@ private Connection con;
 private PreparedStatement ps;
 private ResultSet rs;
 private String info = "Nicht verbunden";
+
+private Calendar cal = Calendar.getInstance();
 
 private ArrayList<Mitarbeiter> mitarbeiter = null;	
 
@@ -133,6 +138,40 @@ private ArrayList<Mitarbeiter> mitarbeiter = null;
 		}	
 	return e;
 	}
+	
+	@Override
+	public SQLException setAZVDaten(AZVDatensatz datenSatz)
+	{
+	SQLException e = null;	
+	boolean result = false;
+		try 
+		{
+		ps = con.prepareStatement(PreparedStatements.INSERT_AZV);
+		ps.setInt(1, datenSatz.getPersonalNummer());
+		ps.setInt(2, datenSatz.getiTeam());
+		ps.setDate(3, datenSatz.getBerichtsMonatSQL());
+		
+			if (datenSatz.getKostenstelle() != null)
+			{
+			ps.setString(4, datenSatz.getKostenstelle());
+			ps.setNull(5, Types.NULL);
+			}
+			else
+			{
+			ps.setNull(4, Types.NULL);
+			ps.setString(5, datenSatz.getKostentraeger());
+			}
+		ps.setInt(6, datenSatz.getProzentanteil());
+			
+		result = ps.execute();
+		} 
+		catch (SQLException exception) 
+		{
+		exception.printStackTrace();
+		e = exception;
+		}	
+	return e;
+	}		
 
 	@Override
 	public void setMitarbeiter(ArrayList<String[]> fields) 
@@ -197,5 +236,152 @@ private ArrayList<Mitarbeiter> mitarbeiter = null;
 	return result;
 	}
 
+	@Override
+	public boolean existsKostenstelle(String kostenStelle)
+	{
+	boolean exists = false;	
+		try 
+		{
+			if (rs != null)
+			{
+			rs.close();
+			rs = null;
+			}	
+		ps = con.prepareStatement(PreparedStatements.SELECT_COUNT_KOSTENSTELLE);
+		ps.setString(1, kostenStelle);
+		rs = ps.executeQuery();
+		rs.next();
+			
+		exists = rs.getInt(PreparedStatements.COUNT_COLUMN) > 0 ? true : false;
+		} 
+		catch (SQLException e) 
+		{
+		e.printStackTrace();
+		}	
+		finally
+		{
+			try
+			{
+			rs.close();
+			if (rs != null)	{rs = null;}
+			}
+			catch (SQLException e)
+			{
+			e.printStackTrace();
+			}	
+		}
+	return exists;
+	}
 
+	@Override
+	public boolean existsKostentraeger(String kostenTraeger)
+	{
+	boolean exists = false;	
+		try 
+		{
+			if (rs != null)
+			{
+			rs.close();
+			rs = null;
+			}	
+		ps = con.prepareStatement(PreparedStatements.SELECT_COUNT_KOSTENTRAEGER);
+		ps.setString(1, kostenTraeger);
+		rs = ps.executeQuery();
+		rs.next();
+			
+		exists = rs.getInt(PreparedStatements.COUNT_COLUMN) > 0 ? true : false;
+		} 
+		catch (SQLException e) 
+		{
+		e.printStackTrace();
+		}	
+		finally
+		{
+			try
+			{
+			rs.close();
+			if (rs != null)	{rs = null;}
+			}
+			catch (SQLException e)
+			{
+			e.printStackTrace();
+			}	
+		}
+	return exists;
+	}
+
+	@Override
+	public SQLException setKostenstelle(String kostenStelle, String kostenStellenBezeichnung)
+	{
+	SQLException e = null;	
+	boolean result = false;
+	// TODO: von Datum wird bisher nur mit Dummy = 01.01.2012 befüllt
+	cal.set(2012, Calendar.JANUARY, 1);
+		try 
+			{
+			ps = con.prepareStatement(PreparedStatements.INSERT_KOSTENSTELLE);
+			ps.setString(1, kostenStelle);
+			ps.setString(2, kostenStellenBezeichnung);
+			ps.setDate(3, new Date(cal.getTimeInMillis()));
+			
+			result = ps.execute();
+			} 
+			catch (SQLException exception) 
+			{
+			exception.printStackTrace();
+			e = exception;
+			}	
+	return e;
+	}
+	
+	@Override
+	public SQLException setKostentraeger(String kostenTraeger, String kostenTraegerBezeichnung)
+	{
+	SQLException e = null;	
+	boolean result = false;
+	// TODO: von Datum wird bisher nur mit Dummy = 01.01.2012 befüllt
+	cal.set(2012, Calendar.JANUARY, 1);
+		try 
+			{
+			ps = con.prepareStatement(PreparedStatements.INSERT_KOSTENTRAEGER);
+			ps.setString(1, kostenTraeger);
+			ps.setString(2, kostenTraegerBezeichnung);
+			ps.setDate(3, new Date(cal.getTimeInMillis()));
+			
+			result = ps.execute();
+			} 
+			catch (SQLException exception) 
+			{
+			exception.printStackTrace();
+			e = exception;
+			}	
+	return e;
+	}
+
+	@Override
+	public SQLException setDatenimport(String name, String pfad, int anzahlDaten, Date berichtsMonat, String datenQuelle)
+	{
+	// [Importdatum],[Dateiname],[Pfad],[AnzahlDaten],[Berichtsmonat],[Datenquelle]	
+	SQLException e = null;	
+	boolean result = false;
+	System.out.println(pfad);
+		try 
+		{
+		ps = con.prepareStatement(PreparedStatements.INSERT_DATENIMPORT);
+		ps.setDate(1, new java.sql.Date(System.currentTimeMillis()));
+		ps.setString(2, name);
+		ps.setString(3, pfad);
+		ps.setInt(4, anzahlDaten);
+		ps.setDate(5, berichtsMonat);
+		ps.setString(6, datenQuelle);
+				
+		result = ps.execute();
+		} 
+		catch (SQLException exception) 
+		{
+		exception.printStackTrace();
+		e = exception;
+		}	
+	return e;
+	}
 }
--- de.hannit.fsch.klr.dataservice.mssql/src/de/hannit/fsch/klr/dataservice/mssql/PreparedStatements.java
+++ de.hannit.fsch.klr.dataservice.mssql/src/de/hannit/fsch/klr/dataservice/mssql/PreparedStatements.java
@@ -9,9 +9,19 @@ package de.hannit.fsch.klr.dataservice.mssql;
  */
 public interface PreparedStatements 
 {
+public static final String COUNT_COLUMN = "Anzahl";
+public static final String INSERT_DATENIMPORT = "INSERT INTO [dbo].[Datenimporte] ([ID], [Importdatum],[Dateiname],[Pfad],[AnzahlDaten],[Berichtsmonat],[Datenquelle])  VALUES (NEWID(),?,?,?,?,?,?)";
+
 public static final String INSERT_MITARBEITER = "INSERT INTO Mitarbeiter ([PNr],[Benutzer],[Nachname],[Vorname]) VALUES (?,?,?,?)";
 public static final String SELECT_MITARBEITER = "SELECT * FROM [dbo].[Mitarbeiter]";
 public static final String SELECT_MITARBEITER_PERSONALNUMMER = "SELECT * FROM [dbo].[Mitarbeiter] WHERE [PNr] = ?";
+
+public static final String SELECT_KOSTENSTELLE = "SELECT * FROM [dbo].[Kostenstellen] WHERE Kostenstelle = ?";
+public static final String INSERT_KOSTENSTELLE = "INSERT INTO [dbo].[Kostenstellen] ([Kostenstelle],[Bezeichnung],[von]) VALUES (?, ?, ?)";
+public static final String SELECT_COUNT_KOSTENSTELLE = "SELECT COUNT([Kostenstelle]) AS Anzahl FROM [dbo].[Kostenstellen]	WHERE [Kostenstelle] = ?";
+public static final String SELECT_COUNT_KOSTENTRAEGER = "SELECT COUNT([Kostentraeger]) AS Anzahl FROM [dbo].[Kostentraeger]	WHERE [Kostentraeger] = ?";
+public static final String INSERT_KOSTENTRAEGER = "INSERT INTO [dbo].[Kostentraeger] ([Kostentraeger],[Bezeichnung],[von])VALUES (?, ?, ?)";
+public static final String INSERT_AZV = "INSERT INTO [dbo].[Arbeitszeitanteile] ([Mitarbeiter_PNR],[TeamNR],[Berichtsmonat],[Kostenstelle],[Kostentraeger],[Prozentanteil]) VALUES (?, ?, ?, ?, ?, ?)";
 
 public static final String INSERT_LOGA = "INSERT INTO [dbo].[LoGa] ([Mitarbeiter_PNR], [Berichtsmonat], [Brutto], [Tarifgruppe], [Tarifstufe], [Stellenanteil]) VALUES (?, ?, ?, ?, ?, ?)";
 }
